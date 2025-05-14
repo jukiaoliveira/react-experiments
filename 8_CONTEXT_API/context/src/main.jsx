9b96593b7bfe3d9fafc2cc0325317988a16491e7
jukiaoliveira <jurafaoliveira@icloud.com>
2025-05-14 14:22:22 -0300

    feat: add provider creation example and implementation

---
+++ 8_CONTEXT_API/context/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import Home from "./routes/Home.jsx";
+import Contact from "./routes/Contact.jsx";
+
+// 2 - Criando o provider
+import { CounterContextProvider } from "./context/CounterContext.jsx";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CounterContextProvider>
+      <RouterProvider router={router} />
+    </CounterContextProvider>
+  </StrictMode>
+);
