3c1cf2c5d2faa6ee6122dd95ac155366605631bb
jukiaoliveira <jurafaoliveira@icloud.com>
2025-05-22 14:43:36 -0300

    Consultando e alterando valor

--- 8_CONTEXT_API/context/src/routes/Contact.jsx
+++ 8_CONTEXT_API/context/src/routes/Contact.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+// 3 - alterando valor do contexto
+import {  useContext } from 'react';
+import { CounterContext } from "../context/CounterContext"
 
 const Contact = () => {
+    const { counter } = useContext(CounterContext);
+
     return (
         <div>
             <h1>Página de contato</h1>
+            <p>Valor do counter: {counter}</p>
         </div>
     );
 };
