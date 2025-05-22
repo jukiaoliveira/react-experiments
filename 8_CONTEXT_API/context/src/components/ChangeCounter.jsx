ff64ee3e37924a3b39933a8cde7543e80c7d7afb
jukiaoliveira <jurafaoliveira@icloud.com>
2025-05-22 14:55:31 -0300

    Refatorando com Hook

--- 8_CONTEXT_API/context/src/components/ChangeCounter.jsx
+++ 8_CONTEXT_API/context/src/components/ChangeCounter.jsx
@@ -2,8 +2,11 @@ import { useContext } from "react";
 
 import { CounterContext } from "../context/CounterContext";
 
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const ChangeCounter = () => {
-    const { counter, setCounter } = useContext(CounterContext);
+    const { counter, setCounter } = useCounterContext();
 
     return (
         <div>
--- 8_CONTEXT_API/context/src/routes/Contact.jsx
+++ 8_CONTEXT_API/context/src/routes/Contact.jsx
@@ -2,8 +2,11 @@
 import {  useContext } from 'react';
 import { CounterContext } from "../context/CounterContext"
 
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Contact = () => {
-    const { counter } = useContext(CounterContext);
+    const { counter } = useCounterContext();
 
     return (
         <div>
